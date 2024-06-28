a0ccaf728ef6a3bb27a167fa7170f058944657ac
TazoEdisherashvili <edisherashvilitazo28@gmail.com>
2024-06-28 19:23:15 +0400

    commit

---
+++ Day 40/homework/homework00.py
@@ -0,0 +1,9 @@
+# მომხამრებელს შეეკითხეთ 3 სხვადასხვა სახელი, ეს სახელები დაამატეთ სიაში,შემდეგ კი დაბეჭდეთ სიის ელემენტები
+
+name1 = input("Enter first name: ")
+name2 = input("Enter second name: ")
+name3 = input("Enter third name: ")
+
+name_list = [name1, name2, name3]
+
+print(name_list[0], name_list[1], name_list[2])
