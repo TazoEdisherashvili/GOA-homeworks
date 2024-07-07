e38ef922f41491ea09a5840c656a71821c4d42f0
TazoEdisherashvili <edisherashvilitazo28@gmail.com>
2024-07-07 15:02:19 +0400

    commit

--- Day 38/homework/homework.py
+++
@@ -1,7 +0,0 @@
-# ნასწავლ მასალაზე ივარჯიშეთ სიებზე და slicing
-
-                                # slicing & sia
-slicing=["1, 2, 3, 4, 5, 6, 7, 8,"]
-
-print[slicing[:3]]
-print[slicing[3:7]]
---
+++ Day 44/classwork/classwork00.py
@@ -0,0 +1,5 @@
+# Print all integers from 0 to 20 inclusive using for loop.
+# დაპრინტეთ ყველა რიცხვი 0-დან 20-ის ჩათვლით for loop-ის გამოყენებით.
+
+for num in range(21):
+    print(num)
